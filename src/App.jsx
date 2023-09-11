174bd38616d14e8489ea11c527bce520a0a425bf
Myroslava Shevska <miroslavashevskaya12@gmail.com>
2023-09-11 13:55:26 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import {useDispatch, useSelector, useStore} from "react-redux";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    const dispatch = useDispatch()
+    const cash = useSelector(state => state.cash)
+    console.log(cash)
+
+
+    const addCash = (cash) => {
+        dispatch({type: "ADD_CASH", payload: cash})
+    }
+
+    const getCash = (cash) => {
+        dispatch({type: "GET_CASH", payload: cash})
+    }
+    return (
+        <div className = {'app'}>
+            <div style = {{fontSize: "5rem"}}>{cash}</div>
+            <div style = {{display: "flex"}}>
+                <button onClick = {() => addCash(Number(prompt()))}>Пополнить счет</button>
+                <button onClick = {() => getCash(Number(prompt()))}>Снять со счета</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom'; // исправлено на правильный импорт
+
 import App from './App';
-import {createStore} from "redux";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-const reducer =(state, )
+const defaultState = {
+    cash: 0,
+}
 
-const store = createStore()
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
+//action = {type: "", payload: ""}
+const reducer = (state = defaultState, action) => {
+    switch (action.type) {
+        case "ADD_CASH":
+            return { ...state, cash: state.cash + action.payload }
+        case "GET_CASH":
+            return { ...state, cash: state.cash - action.payload }
+        default:
+            return state
+    }
+}
 
+const store = createStore(reducer);
+
+ReactDOM.render(  // исправлено на правильный вызов
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
-
-
